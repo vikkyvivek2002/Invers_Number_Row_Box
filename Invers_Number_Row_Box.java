4bc3cf9d77d3a5ac5ac60677ae33e9342a114deb
vikkyvivek2002 <84318895+vikkyvivek2002@users.noreply.github.com>
2021-06-07 23:36:00 +0530

    Add files via upload

---
+++ Invers_Number_Row_Box.java
@@ -0,0 +1,14 @@
+class Invers_Number_Row_Box
+{
+public static void main(String[] args)
+{
+ for(int i=5; i>=1; i--)
+{
+  for(int j=5; j>=1; j--)
+{
+System.out.print(j);
+}
+System.out.println();
+}
+}
+}
